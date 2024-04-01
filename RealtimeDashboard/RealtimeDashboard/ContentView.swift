9e95a2bc3554918820c73c7ccfc79b41b3345e87
Bilven Parikh <bparikh@gategroup.com>
2024-04-01 21:03:00 +0530

    Initial Project
    Initial Project Commit

---
+++ RealtimeDashboard/RealtimeDashboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealtimeDashboard
+//
+//  Created by Bilven Parikh on 01/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealtimeDashboard/RealtimeDashboard/RealtimeDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeDashboardApp.swift
+//  RealtimeDashboard
+//
+//  Created by Bilven Parikh on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
